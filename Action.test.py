15f021e53a85d0633b68f8b353170795e94a5bfd
Vincent Jacques <vincent@vincent-jacques.net>
2012-01-31 07:38:20 +0100

    Initiate the project

---
+++ Action.test.py
@@ -0,0 +1,10 @@
+import unittest
+from MockMockMock import Mock
+
+from ActionTree import Action
+
+class TestCase( unittest.TestCase ):
+    def setUp( self ):
+        unittest.TestCase.setUp( self )
+
+unittest.main()
