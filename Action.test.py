b1c8067460fd0a3765e4c6137f35310d4c795c14
Vincent Jacques <vincent@vincent-jacques.net>
2012-01-31 07:45:34 +0100

    Actually execute something

--- Action.test.py
+++ Action.test.py
@@ -1,10 +1,19 @@
 import unittest
+import time
 from MockMockMock import Mock
 
 from ActionTree import Action
 
-class TestCase( unittest.TestCase ):
+class SimpleExecution( unittest.TestCase ):
     def setUp( self ):
         unittest.TestCase.setUp( self )
+        self.mock = Mock( "a" )
+
+    def tearDown( self ):
+        self.mock.tearDown()
+
+    def test( self ):
+        self.mock.expect()
+        Action( self.mock.object ).execute()
 
 unittest.main()
--- ActionTree.py
+++ ActionTree.py
@@ -1,2 +1,6 @@
 class Action:
-    pass
+    def __init__( self, execute ):
+        self.__execute = execute
+
+    def execute( self ):
+        self.__execute()
