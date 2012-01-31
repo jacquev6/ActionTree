c9e1fdf0cd167f2cc8b1e38f7b72cd5d126b2377
Vincent Jacques <vincent@vincent-jacques.net>
2012-01-31 07:53:47 +0100

    Test begin and end of action

--- Action.test.py
+++ Action.test.py
@@ -4,16 +4,28 @@ from MockMockMock import Mock
 
 from ActionTree import Action
 
-class SimpleExecution( unittest.TestCase ):
+class ExecuteMock:
+    def __init__( self, mock ):
+        self.__mock = mock
+
+    def __call__( self ):
+        self.__mock.begin()
+        time.sleep( 0.1 )
+        self.__mock.end()
+
+class SingleAction( unittest.TestCase ):
     def setUp( self ):
         unittest.TestCase.setUp( self )
         self.mock = Mock( "a" )
+        self.action = Action( ExecuteMock( self.mock.object ) )
 
     def tearDown( self ):
         self.mock.tearDown()
 
     def test( self ):
-        self.mock.expect()
-        Action( self.mock.object ).execute()
+        self.mock.expect.begin()
+        self.mock.expect.end()
+
+        self.action.execute()
 
 unittest.main()
