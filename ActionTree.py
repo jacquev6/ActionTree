8e1b623cbccc41cd4ac031f80a6eba3735a82eed
Vincent Jacques <vincent@vincent-jacques.net>
2012-02-05 09:04:57 +0100

    Remove bad attributes

--- ActionTree.py
+++ ActionTree.py
@@ -25,27 +25,26 @@ class Action:
         return dependencies
 
     def execute( self, threads = 1, keepGoing = False ):
-        self.__condition = threading.Condition()
-        self.__exceptions = []
-        self.__keepGoing = keepGoing
-        threads = [ threading.Thread( target = self.__executeInOneThread ) for i in range( threads ) ]
+        condition = threading.Condition()
+        exceptions = []
+        threads = [ threading.Thread( target = self.__executeInOneThread, args = ( condition, exceptions, keepGoing ) ) for i in range( threads ) ]
         for thread in threads:
             thread.start()
         for thread in threads:
             thread.join()
-        if len( self.__exceptions ) > 0:
-            raise Action.Exception( self.__exceptions )
+        if len( exceptions ) > 0:
+            raise Action.Exception( exceptions )
 
-    def __executeInOneThread( self ):
+    def __executeInOneThread( self, condition, exceptions, keepGoing ):
         while True:
-            with self.__condition:
+            with condition:
                 a = None
                 while a is None:
                     a = self.__getNextActionToExecute()
                     if self.__executed or self.__canceled or self.__failed:
                         return
                     if a is None:
-                        self.__condition.wait()
+                        condition.wait()
                 if any( d.__failed or d.__canceled for d in a.__dependencies ):
                     a.__canceled = True
                 else:
@@ -54,16 +53,16 @@ class Action:
                 try:
                     a.__execute()
                 except Exception, e:
-                    with self.__condition:
+                    with condition:
                         a.__failed = True
-                        self.__exceptions.append( e )
-                        if not self.__keepGoing:
+                        exceptions.append( e )
+                        if not keepGoing:
                             self.__canceled = True
-                with self.__condition:
+                with condition:
                     if not a.__failed:
                         a.__executed = True
                     a.__executing = False
-                    self.__condition.notifyAll()
+                    condition.notifyAll()
 
     def __getNextActionToExecute( self ):
         if self.__executed or self.__executing or self.__failed or self.__canceled:
