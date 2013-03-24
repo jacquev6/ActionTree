2a445d168123256bbe1a66c4b9be0759185074ed
Vincent Jacques <vincent@vincent-jacques.net>
2013-03-24 10:34:40 +0100

    Simplify test for multi-threaded execution

--- ActionTree/tests/MultiThreadedExecution.py
+++ ActionTree/tests/MultiThreadedExecution.py
@@ -21,45 +21,6 @@ import MockMockMock
 from ActionTree import Action
 
 
-class TestCase(unittest.TestCase):
-    def setUp(self):
-        unittest.TestCase.setUp(self)
-        self.mocks = MockMockMock.Engine()
-        self.__mocks = dict()
-        self.__actions = dict()
-        for name in "abcdef":
-            self.__addMock(name)
-
-    def __addMock(self, name):
-        m = self.mocks.create(name)
-        a = Action(self.callableFromMock(m.object), name)
-        self.__mocks[name] = m
-        self.__actions[name] = a
-
-    def callableFromMock(self, m):
-        return m
-
-    def tearDown(self):
-        self.mocks.tearDown()
-
-    def addDependency(self, a, b):
-        self.__actions[a].addDependency(self.__actions[b])
-
-    def getMock(self, name):
-        return self.__mocks[name]
-
-    def getAction(self, name):
-        return self.__actions[name]
-
-    @property
-    def unordered(self):
-        return self.mocks.unordered
-
-    @property
-    def optional(self):
-        return self.mocks.optional
-
-
 class ExecuteMock:
     def __init__(self, mock):
         self.__mock = mock
@@ -73,100 +34,134 @@ class ExecuteMock:
             self.__mock.end()
 
 
-class ThreadingTestCase:
+class MultiThreadedExecution(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.mocks = MockMockMock.Engine()
+
+    def __createMockedAction(self, name):
+        mock = self.mocks.create(name + "Mock")
+        action = Action(ExecuteMock(mock.object), name)
+        return action, mock
+
+    def tearDown(self):
+        self.mocks.tearDown()
+
     def testManyDependencies(self):
-        dependencies = "bcd"
-        for name in dependencies:
-            self.addDependency("a", name)
+        #     a
+        #    /|\
+        #   / | \
+        #  b  c  d
 
-        self.expectManyDependencies(dependencies)
+        a, aMock = self.__createMockedAction("a")
+        b, bMock = self.__createMockedAction("b")
+        c, cMock = self.__createMockedAction("c")
+        d, dMock = self.__createMockedAction("d")
+        a.addDependency(b)
+        a.addDependency(c)
+        a.addDependency(d)
 
-        self.executeAction(self.getAction("a"))
+        with self.mocks.unordered:
+            bMock.expect.begin()
+            cMock.expect.begin()
+            dMock.expect.begin()
+        with self.mocks.unordered:
+            bMock.expect.end()
+            cMock.expect.end()
+            dMock.expect.end()
+        aMock.expect.begin()
+        aMock.expect.end()
+
+        a.execute(jobs=3)
 
     def testDeepDependencies(self):
-        self.addDependency("a", "b")
-        self.addDependency("b", "c")
-        self.addDependency("c", "d")
-        self.addDependency("d", "e")
-        self.addDependency("e", "f")
+        #  a
+        #  |
+        #  b
+        #  |
+        #  c
+        #  |
+        #  d
+        #  |
+        #  e
+        #  |
+        #  f
 
-        self.expectDeepDependencies()
+        a, aMock = self.__createMockedAction("a")
+        b, bMock = self.__createMockedAction("b")
+        c, cMock = self.__createMockedAction("c")
+        d, dMock = self.__createMockedAction("d")
+        e, eMock = self.__createMockedAction("e")
+        f, fMock = self.__createMockedAction("f")
+        a.addDependency(b)
+        b.addDependency(c)
+        c.addDependency(d)
+        d.addDependency(e)
+        e.addDependency(f)
 
-        self.executeAction(self.getAction("a"))
+        fMock.expect.begin()
+        fMock.expect.end()
+        eMock.expect.begin()
+        eMock.expect.end()
+        dMock.expect.begin()
+        dMock.expect.end()
+        cMock.expect.begin()
+        cMock.expect.end()
+        bMock.expect.begin()
+        bMock.expect.end()
+        aMock.expect.begin()
+        aMock.expect.end()
+
+        a.execute(jobs=3)
 
     def testDiamondDependencies(self):
-        self.addDependency("a", "b")
-        self.addDependency("a", "c")
-        self.addDependency("b", "d")
-        self.addDependency("c", "d")
+        #     a
+        #    / \
+        #   b   c
+        #    \ /
+        #     d
 
-        self.expectDiamondDependencies()
+        a, aMock = self.__createMockedAction("a")
+        b, bMock = self.__createMockedAction("b")
+        c, cMock = self.__createMockedAction("c")
+        d, dMock = self.__createMockedAction("d")
+        a.addDependency(b)
+        a.addDependency(c)
+        b.addDependency(d)
+        c.addDependency(d)
 
-        self.executeAction(self.getAction("a"))
+        dMock.expect.begin()
+        dMock.expect.end()
+        with self.mocks.unordered:
+            bMock.expect.begin()
+            cMock.expect.begin()
+        with self.mocks.unordered:
+            bMock.expect.end()
+            cMock.expect.end()
+        aMock.expect.begin()
+        aMock.expect.end()
+
+        a.execute(jobs=3)
 
     def testHalfDiamondDependency(self):
-        self.addDependency("a", "b")
-        self.addDependency("a", "d")
-        self.addDependency("b", "d")
+        #     a
+        #    /|
+        #   b |
+        #    \|
+        #     d
 
-        self.expectHalfDiamondDependencies()
+        a, aMock = self.__createMockedAction("a")
+        b, bMock = self.__createMockedAction("b")
+        d, dMock = self.__createMockedAction("d")
+        a.addDependency(b)
+        a.addDependency(d)
+        b.addDependency(d)
 
-        self.executeAction(self.getAction("a"))
+        dMock.expect.begin()
+        dMock.expect.end()
+        bMock.expect.begin()
+        bMock.expect.end()
+        aMock.expect.begin()
+        aMock.expect.end()
 
-
-class MultiThreadedExecution(TestCase, ThreadingTestCase):
-    def callableFromMock(self, m):
-        return ExecuteMock(m)
-
-    def executeAction(self, action):
-        action.execute(jobs=3)
-
-    def __expectBegin(self, name):
-        self.getMock(name).expect.begin()
-
-    def __expectEnd(self, name):
-        self.getMock(name).expect.end()
-
-    def expectManyDependencies(self, dependencies):
-        with self.unordered:
-            for name in dependencies:
-                self.__expectBegin(name)
-        with self.unordered:
-            for name in dependencies:
-                self.__expectEnd(name)
-        self.__expectBegin("a")
-        self.__expectEnd("a")
-
-    def expectDiamondDependencies(self):
-        self.__expectBegin("d")
-        self.__expectEnd("d")
-        with self.unordered:
-            self.__expectBegin("b")
-            self.__expectBegin("c")
-        with self.unordered:
-            self.__expectEnd("b")
-            self.__expectEnd("c")
-        self.__expectBegin("a")
-        self.__expectEnd("a")
-
-    def expectHalfDiamondDependencies(self):
-        self.__expectBegin("d")
-        self.__expectEnd("d")
-        self.__expectBegin("b")
-        self.__expectEnd("b")
-        self.__expectBegin("a")
-        self.__expectEnd("a")
-
-    def expectDeepDependencies(self):
-        self.__expectBegin("f")
-        self.__expectEnd("f")
-        self.__expectBegin("e")
-        self.__expectEnd("e")
-        self.__expectBegin("d")
-        self.__expectEnd("d")
-        self.__expectBegin("c")
-        self.__expectEnd("c")
-        self.__expectBegin("b")
-        self.__expectEnd("b")
-        self.__expectBegin("a")
-        self.__expectEnd("a")
+        a.execute(jobs=3)
