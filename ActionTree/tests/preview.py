798c0ca1d5b1fb6957704e7b92c4f5ef734a4d5c
Vincent Jacques <vincent@vincent-jacques.net>
2015-05-14 07:55:13 +0200

    Fix branch coverage
    Not investigated deeply: "lambda: 0" was generating a partial branch
    coverage.

--- ActionTree/tests/preview.py
+++ ActionTree/tests/preview.py
@@ -7,11 +7,15 @@ import unittest
 from ActionTree import Action
 
 
+def noop():  # pragma no cover (Test code)
+    pass
+
+
 class PreviewTestCase(unittest.TestCase):
     def test_get_dependencies_and_labels_are_not_only_equal_but_same(self):
         bLabel = ("b",)
-        a = Action(lambda: 0, "a")
-        b = Action(lambda: 0, bLabel)
+        a = Action(noop, "a")
+        b = Action(noop, bLabel)
         a.add_dependency(b)
 
         otherB, = a.get_dependencies()
@@ -19,22 +23,22 @@ class PreviewTestCase(unittest.TestCase):
         self.assertIs(otherB.label, bLabel)
 
     def test_simple_preview(self):
-        a = Action(lambda: 0, "a")
+        a = Action(noop, "a")
         self.assertEqual(a.get_preview(), ["a"])
 
     def test_typed_label(self):
-        a = Action(lambda: 0, ("a", "curious", "label", 42))
+        a = Action(noop, ("a", "curious", "label", 42))
         self.assertEqual(a.get_preview(), [("a", "curious", "label", 42)])
 
     def test_none_label(self):
-        a = Action(lambda: 0, None)
+        a = Action(noop, None)
         self.assertEqual(a.get_preview(), [])
 
     def test_deep_dependency(self):
-        a = Action(lambda: 0, "a")
-        b = Action(lambda: 0, "b")
-        c = Action(lambda: 0, "c")
-        d = Action(lambda: 0, "d")
+        a = Action(noop, "a")
+        b = Action(noop, "b")
+        c = Action(noop, "c")
+        d = Action(noop, "d")
         a.add_dependency(b)
         b.add_dependency(c)
         c.add_dependency(d)
@@ -42,10 +46,10 @@ class PreviewTestCase(unittest.TestCase):
         self.assertEqual(a.get_preview(), ["d", "c", "b", "a"])
 
     def test_deep_dependency_with_duplicated_label(self):
-        a = Action(lambda: 0, "label")
-        b = Action(lambda: 0, "label")
-        c = Action(lambda: 0, "label")
-        d = Action(lambda: 0, "label")
+        a = Action(noop, "label")
+        b = Action(noop, "label")
+        c = Action(noop, "label")
+        d = Action(noop, "label")
         a.add_dependency(b)
         b.add_dependency(c)
         c.add_dependency(d)
@@ -59,10 +63,10 @@ class PreviewTestCase(unittest.TestCase):
         #    \ /
         #     d
 
-        a = Action(lambda: 0, "a")
-        b = Action(lambda: 0, "b")
-        c = Action(lambda: 0, "c")
-        d = Action(lambda: 0, "d")
+        a = Action(noop, "a")
+        b = Action(noop, "b")
+        c = Action(noop, "c")
+        d = Action(noop, "d")
         a.add_dependency(b)
         a.add_dependency(c)
         b.add_dependency(d)
