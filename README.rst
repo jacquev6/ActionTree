1a697359b57ae743366da7435de7592809252553
Vincent Jacques <vincent@vincent-jacques.net>
2018-11-11 11:36:05 +0100

    Use devlpr

--- README.rst
+++ README.rst
@@ -1,10 +1,23 @@
+.. GENI: intro
+.. GENERATED SECTION, MANUAL EDITS WILL BE LOST
+
 *ActionTree* is a Python (2.7 and 3.5+) library to execute (long) actions in parallel, respecting dependencies between those actions.
 You create a dependency graph of actions to be executed and then call the ``execute`` function on its root.
+
+.. END OF GENERATED SECTION
+
+.. GENI: info
+.. GENERATED SECTION, MANUAL EDITS WILL BE LOST
 
 It's licensed under the `MIT license <http://choosealicense.com/licenses/mit/>`_.
 It's available on the `Python package index <http://pypi.python.org/pypi/ActionTree>`_.
 Its `documentation <http://jacquev6.github.io/ActionTree>`_
 and its `source code <https://github.com/jacquev6/ActionTree>`_ are on GitHub.
+
+.. END OF GENERATED SECTION
+
+.. GENI: badges
+.. GENERATED SECTION, MANUAL EDITS WILL BE LOST
 
 Questions? Remarks? Bugs? Want to contribute? `Open an issue <https://github.com/jacquev6/ActionTree/issues>`_!
 
@@ -43,6 +56,8 @@ Questions? Remarks? Bugs? Want to contribute? `Open an issue <https://github.com
 
 .. image:: https://img.shields.io/github/stars/jacquev6/ActionTree.svg
     :target: https://github.com/jacquev6/ActionTree/stargazers
+
+.. END OF GENERATED SECTION
 
 Quick start
 ===========
--- run_development_cycle.sh
+++ run_development_cycle.sh
@@ -2,35 +2,146 @@
 
 # Copyright 2017-2018 Vincent Jacques <vincent@vincent-jacques.net>
 
-set -o errexit
+# GENI: prologue
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+set -o errexit -o pipefail
+IFS=$'\n\t'
 
-clear
+PROJECT_ROOT=$(pwd)
 
-coverage2 erase
-coverage2 run --include=ActionTree* setup.py test --quiet
-coverage2 combine
-coverage2 report
+SHOW_IN_BROWSER=false
+function show_in_browser {
+  echo
+  echo "$1: $PROJECT_ROOT/$2"
+  echo
+  if $SHOW_IN_BROWSER
+  then
+    python -m webbrowser -t file://$PROJECT_ROOT/$2
+  fi
+}
 
-coverage3 erase
-coverage3 run --include=build/lib/ActionTree* setup.py test --quiet
-coverage3 combine
-coverage3 report
+DO_DOC=true
+DO_DOCTESTS=true
+DO_PIP_INSTALL=true
 
-python2 setup.py build_sphinx --builder=doctest
+while [[ "$#" > 0 ]]
+do
+  case $1 in
+    -wb|--web-browser)
+      SHOW_IN_BROWSER=true
+      ;;
+    --skip-doc)
+      DO_DOC=false
+      ;;
+    --skip-doctests)
+      DO_DOCTESTS=false
+      ;;
+    --skip-pip-install)
+      DO_PIP_INSTALL=false
+      ;;
+    -q|--quick)
+      DO_DOC=false
+      DO_DOCTESTS=false
+      DO_PIP_INSTALL=false
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1;;
+  esac
+  shift
+done
+# END OF GENERATED SECTION
 
-python2 setup.py check --strict --metadata --restructuredtext
 
-pycodestyle --max-line-length=120 ActionTree *.py doc/conf.py
+# GENI: install_dependencies
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+if ! [ -d _builds/venv2.dev_with_oldest_versions ]; then virtualenv --always-copy --python=python2 _builds/venv2.dev_with_oldest_versions; fi
+. _builds/venv2.dev_with_oldest_versions/bin/activate
+pip install --upgrade pip setuptools wheel coverage sphinx
+pip install --upgrade graphviz matplotlib mock
+deactivate
 
-python2 setup.py build_sphinx
-rm -rf docs
-cp -r build/sphinx/html docs
+if ! [ -d _builds/venv3.dev_with_newest_versions ]; then virtualenv --always-copy --python=python3 _builds/venv3.dev_with_newest_versions; fi
+. _builds/venv3.dev_with_newest_versions/bin/activate
+pip install --upgrade pip setuptools wheel coverage sphinx
+pip install --upgrade graphviz matplotlib mock
+deactivate
+# END OF GENERATED SECTION
+
+
+# GENI: run_tests
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+. _builds/venv2.dev_with_oldest_versions/bin/activate
+coverage erase
+coverage run --branch --include="ActionTree*" setup.py test --quiet
+coverage combine
+coverage report
 echo
-echo "See documentation in $(pwd)/docs/index.html"
-echo
+coverage html --directory=_builds/coverage_with_oldest_versions
+show_in_browser "Coverage details (with oldest versions)" _builds/coverage_with_oldest_versions/index.html
+coverage erase
+if $DO_DOCTESTS; then python setup.py build_sphinx --builder=doctest; fi
+deactivate
 
-# python3 setup.py install
+. _builds/venv3.dev_with_newest_versions/bin/activate
+coverage erase
+coverage run --branch --include="build/lib/ActionTree*" setup.py test --quiet
+coverage combine
+coverage report
+echo
+coverage html --directory=_builds/coverage_with_newest_versions
+show_in_browser "Coverage details (with newest versions)" _builds/coverage_with_newest_versions/index.html
+coverage erase
+deactivate
+# END OF GENERATED SECTION
+
+
+# GENI: check_code
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+python3 setup.py check --strict --metadata --restructuredtext
+pycodestyle --max-line-length=120 $(git ls-files "*.py")
+# END OF GENERATED SECTION
+
+
+# GENI: documentation
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+if $DO_DOC
+then
+  . _builds/venv3.dev_with_newest_versions/bin/activate
+  python setup.py build_sphinx
+  deactivate
+  rm -rf docs
+  cp -r build/sphinx/html docs
+  show_in_browser "Documentation" docs/index.html
+fi
+# END OF GENERATED SECTION
+
+
+# GENI: install
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+if $DO_PIP_INSTALL
+then
+  rm -rf _builds/venv2.install
+  virtualenv --always-copy --python=python2 _builds/venv2.install
+  . _builds/venv2.install/bin/activate
+  pip install .
+  deactivate
+
+  rm -rf _builds/venv3.install
+  virtualenv --always-copy --python=python3 _builds/venv3.install
+  . _builds/venv3.install/bin/activate
+  pip install .
+  deactivate
+fi
+# END OF GENERATED SECTION
+
+
 # cd demo
 # ./demo.py
 
+
+# GENI: epilogue
+# GENERATED SECTION, MANUAL EDITS WILL BE LOST
+echo
 echo "Development cycle OK"
+# END OF GENERATED SECTION
