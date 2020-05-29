edb2300ab5b3cd0458d5400d79e71b63e80e2412
Vincent Jacques <vincent@vincent-jacques.net>
2020-05-29 08:35:55 +0200

    Measure test coverage

--- development/tests/entrypoint.sh
+++ development/tests/entrypoint.sh
@@ -2,5 +2,22 @@
 
 set -o errexit
 
-# @todo Measure coverage, generate HTML report
-python -m unittest discover --pattern "*.py" --start-directory ActionTree --top-level-directory .
+
+coverage erase
+trap "coverage erase" EXIT
+
+# Because of multiprocessing:
+#   --branch option must be passed as rc file
+#   --rcfile option must be passed as env variable
+export COVERAGE_RCFILE=development/tests/coveragerc
+coverage run -m unittest discover --pattern "*.py" --start-directory ActionTree --top-level-directory .
+coverage combine
+
+coverage html --directory="htmlcov/$(python --version)"
+
+coverage report --skip-covered --fail-under=100
+
+echo
+echo "Detailed coverage report: htmlcov/$(python --version)/index.html"
+echo
+echo "Tests and coverage OK"
